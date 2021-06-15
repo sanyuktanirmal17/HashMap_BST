3f1e75a3d655e43ebb9906bc098e6865957a5ebb
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-16 00:15:46 +0530

    Added: Put() method for Key and val

--- src/Runner.java
+++ src/Runner.java
@@ -10,8 +10,13 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		Map<String,Integer> mymap=new Map<>();
-		
+		Map<String,Integer> map=new Map<>();
+		map.put("To", 0);
+		map.put("be", 1);
+		map.put("or", 0);
+		map.put("not",0);
+		map.put("to", 0);
+		map.put("be", 2);
 		
 	}
 
